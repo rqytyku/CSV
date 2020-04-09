befcd13777fbc8e3727fd5e9b6a5694a7b85bad9
Romela Qytyku <rqytyku2@uet.edu.al>
2020-04-09 12:47:18 +0200

    Add project files.

---
+++ CSV/CSVreader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSV
+{
+    class CSVreader
+    {
+        private string _csvFilePath;
+
+        public CSVreader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+        public Country[] ReadFirstCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                sr.ReadLine();
+                for(int i=0; i<nCountries;i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    countries[i] = ReadCountryfroCSV(csvLine);
+                }
+            }
+            return countries;
+        }
+        public Country ReadCountryfroCSV (string csvLine)
+        {
+            string[] parts = csvLine.Split(new char[] { ',' });
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+           int population = int.Parse(parts[3]);
+            return new Country(name, code, region, population);
+            
+        }
+       
+    } 
+}
---
+++ CSV/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\romel\OneDrive\Documents\Book1.csv ";
+            CSVreader reader = new CSVreader(filePath);
+            Country[] countries = reader.ReadFirstCountries(2);
+            foreach (Country country in countries)
+            {
+                //Console.WriteLine($"{country.name}:  {country.population}  ");
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.population).PadLeft(15)}: {country.name}");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+
+ }
+     
